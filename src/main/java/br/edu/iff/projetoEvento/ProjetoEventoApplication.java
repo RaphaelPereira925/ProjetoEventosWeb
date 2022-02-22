06a40163960e83671a05b22873224153ef27f7a6
rapha <rapha@Raphael>
2022-02-22 18:54:21 -0300

    Criação do projeto.

---
+++ src/main/java/br/edu/iff/projetoEvento/ProjetoEventoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iff.projetoEvento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEventoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/iff/projetoEvento/ProjetoEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.projetoEvento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
